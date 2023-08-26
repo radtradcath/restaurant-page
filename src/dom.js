a6ce53474146136a3edcff7b14e8e482dcc4ae07
radtradcath <vinicius.b.bonafini@gmail.com>
2023-08-26 10:58:40 -0300

    Add fonts

--- src/dom.js
+++ src/dom.js
@@ -37,12 +37,15 @@ nav.appendChild(navlink3);
 
 welcomeContainer.classList.add('welcome');
 welcomeContainer.appendChild(welcomeHeader);
+welcomeHeader.textContent = "Hi, this is my new website";
 welcomeContainer.appendChild(welcomeMessage);
 
 
 infoContainer.classList.add('info')
 infoContainer.appendChild(infoPara1);
+infoPara1.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, nesciunt sint vero labore eaque qui."
 infoContainer.appendChild(infoPara2);
+infoPara2.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero placeat reprehenderit eius. Debitis sit laborum, praesentium perferendis maiores ex doloribus molestiae! Maiores minus repellat tenetur voluptas molestias quibusdam optio dolorum."
 
 
 footer.appendChild(footerTM);
